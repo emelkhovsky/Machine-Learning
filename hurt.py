59775f969fcd2198716f42db41fe5186837e96c1
emelkhovsky <emelkhovsky4@mail.ru>
2019-12-19 21:54:21 +0300

    almost the end of processing of json file

--- hurt.py
+++ hurt.py
@@ -1,62 +1,54 @@
 import json
-import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
-def formating_files():
+def for_leads(leads_list, data, j):
+    dop_list_leads = []
+    data_leads = data[j]["Message"]
+    number_symbol = data_leads.find('V5 = ')
+    number_symbol_copy = number_symbol
+    while data_leads[number_symbol_copy] + data_leads[number_symbol_copy + 1] != '->':
+        number_symbol_copy = number_symbol_copy + 1
+    lead_str = data_leads[number_symbol:(number_symbol_copy - 3)]
+    lead_after_split = lead_str.split(',')
+    for f in lead_after_split:
+        dop_list_leads.append(int(f.split(' = ')[1]))
+    print(dop_list_leads)
+    if len(dop_list_leads) < 12:
+        index_patients = patients_list.index(dop_list_leads)
+        patients_list.remove(index_patients)
+        index_sign = signal_list.index(dop_list_leads)
+        patients_list.remove(index_sign)
+    else:
+        leads_list.append(dop_list_leads)
+
+
+def formating_files(diagnoz_list, names_diagnoz, signal_list, patients_list, leads_list):
     with open('data_2033_1.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
         print('ФАЙЛ 1')
         for j in data.keys():
-            print(j)
+            patients_list.append(j)
+            for_leads(leads_list, data, j)
             data_signal = data[j]["Leads"]
             for k in data_signal.keys():
                 signal = data[j]["Leads"][k]['Signal']
-                #graphics(signal)
                 signal_list.append(signal)
-            data_regular_normosystole = data[j]['StructuredDiagnosisDoc']['regular_normosystole']
-            one_list.append(data_regular_normosystole)
-            data_sinus_tachycardia = data[j]['StructuredDiagnosisDoc']['sinus_tachycardia']
-            two_list.append(data_sinus_tachycardia)
-            data_sinus_bradycardia = data[j]['StructuredDiagnosisDoc']['sinus_bradycardia']
-            three_list.append(data_sinus_bradycardia)
-            data_sinus_arrhythmia = data[j]['StructuredDiagnosisDoc']['sinus_arrhythmia']
-            four_list.append(data_sinus_arrhythmia)
-            data_irregular_sinus_rhythm = data[j]['StructuredDiagnosisDoc']['irregular_sinus_rhythm']
-            five_list.append(data_irregular_sinus_rhythm)
+            for name in names_diagnoz:
+                data_diagnoz = data[j]['StructuredDiagnosisDoc'][name]
+                index = names_diagnoz.index(name)
+                diagnoz_list[index].append(data_diagnoz)
 
-
-def create_pandas(one_list,two_list,three_list,four_list,five_list):
-    df = pd.DataFrame({
-        'signal': signal_list[:21:],
-        '1st_in_sin': one_list[:21:],
-        '2nd_in_sin': two_list[:21:],
-        '3d_in_sin': three_list[:21:],
-        '4th_in_sin': four_list[:21:]
-    })
-    return df
-
-def graphics(data):
-    plt.plot(data)
-    plt.show()
-
+diagnoz_list = []
+for i in range(5):
+    diagnoz_list.append([])
+names_diagnoz = ['regular_normosystole', 'sinus_tachycardia', 'sinus_bradycardia', 'sinus_arrhythmia', 'irregular_sinus_rhythm']
 signal_list = []
-one_list = []
-two_list = []
-three_list = []
-four_list = []
-five_list = []
-results_list = []
-formating_files()
-print(one_list)
-print(two_list)
-print(three_list)
-print(four_list)
-print(five_list)
-#print(signal_list)
-df = create_pandas(one_list,two_list,three_list,four_list,five_list)
-print(df['1st_in_sin'])
-for row in df.iterrows():
-    if True in row:
-        print('СИНУСОВЫЙ')
-    else:
-        print('НЕТ')
+patients_list = []
+leads_list = []
+
+
+formating_files(diagnoz_list, names_diagnoz, signal_list, patients_list, leads_list)
+print(leads_list)
+
+
+
